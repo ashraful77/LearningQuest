dc435766ccd8abdbb06d6063aeebc9c84ea9ec27
ashraful77 <116723692+ashraful77@users.noreply.github.com>
2026-09-24 22:17:48 +0530

    Fix handwriting screen graphics layer import

--- app/src/main/java/com/ashraful/learningquest/screens/AbidWriteLettersScreen.kt
+++ app/src/main/java/com/ashraful/learningquest/screens/AbidWriteLettersScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
