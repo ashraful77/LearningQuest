3489551b02a6070a2947f7f21bed4eedcd1e89b8
ashraful77 <116723692+ashraful77@users.noreply.github.com>
2026-09-24 22:30:33 +0530

    Add Color Match navigation

--- app/src/main/java/com/ashraful/learningquest/screens/HomeScreen.kt
+++ app/src/main/java/com/ashraful/learningquest/screens/HomeScreen.kt
@@ -54,6 +54,7 @@ fun HomeScreen() {
         "abid_numbers" -> AbidNumbersScreen { screen = "home" }
         "abid_shapes" -> AbidShapesScreen { screen = "home" }
         "abid_colors" -> AbidColorsScreen { screen = "home" }
+        "abid_color_match" -> AbidColorMatchScreen { screen = "home" }
         "abid_match_letters" -> AbidMatchLettersScreen { screen = "home" }
         "abid_number_match" -> AbidNumberMatchScreen { screen = "home" }
     }
